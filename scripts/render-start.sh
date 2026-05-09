32c2c05392c1abba9cfa8530448f6b24e3aa3c74
hieu <lehieunghiahanh76@gmail.com>
2026-05-09 08:40:05 +0700

    Bind Airflow webserver to render port

--- scripts/render-start.sh
+++ scripts/render-start.sh
@@ -3,4 +3,7 @@ set -euo pipefail
 
 mkdir -p "${AIRFLOW_HOME}"
 
+export AIRFLOW__WEBSERVER__WEB_SERVER_HOST=0.0.0.0
+export AIRFLOW__WEBSERVER__WEB_SERVER_PORT="${PORT:-10000}"
+
 exec airflow standalone
